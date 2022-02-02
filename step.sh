5f73bbaf3d5402b4a9cf8a9b2c69d59a44774b5c
Riyuu, Kage | Ryu | ECMPD <bilal.a.ahmad@rakuten.com>
2022-02-02 15:15:01 +0900

    Fixed output export issue

--- step.sh
+++ step.sh
@@ -91,4 +91,4 @@ ${MAIN_VERSION_AVAILABLE_MARKER} = New main version available (Recomend Update)
 }
 STATS=$(generatePodsStats)
 echo "$STATS"
-envman add --key PODS_USED_STATUS --value "${STATS}"
+envman add --key USED_LIBS_STATUS --value "$STATS"
